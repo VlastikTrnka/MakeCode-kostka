62fe29ec0fbd1a4e009b1bc1ec68fcb1263630b7
VlastikTrnka <91247597+VlastikTrnka@users.noreply.github.com>
2021-11-03 19:32:33 +0100

    update-kostka

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+y = 0
+x = 0 
+def logo():
+    if y == 0:
+            global x
+            x = 0
+            basic.show_string("1-10")
+            global y
+            y = 1
+
+    else:
+            global x
+            x = 0
+            basic.show_string("1-6")
+            global y
+            y = 0
+input.on_logo_event(TouchButtonEvent.PRESSED, logo)
+
+def pressA():
+    if y == 0:
+        global x
+        x = randint(1, 6)
+        for index in range(x):                          
+                            music.play_tone(Note.D3, music.beat(90))
+                            
+                            
+    else:
+        global x
+        x = randint(0, 10)
+        for index in range(x):
+                            music.play_tone(Note.D3, music.beat(90))
+                            
+
+                            
+                            
+
+input.on_button_pressed(Button.A, pressA)
+
+
+
--- main.ts
+++ main.ts
@@ -1,3 +1,35 @@
-basic.forever(function () {
-	
+let y = 0
+let x = 0
+input.onLogoEvent(TouchButtonEvent.Pressed, function logo() {
+    if (y == 0) {
+        
+        x = 0
+        basic.showString("1-10")
+        
+        y = 1
+    } else {
+        
+        x = 0
+        basic.showString("1-6")
+        
+        y = 0
+    }
+    
+})
+input.onButtonPressed(Button.A, function pressA() {
+    let index: number;
+    if (y == 0) {
+        
+        x = randint(1, 6)
+        for (index = 0; index < x; index++) {
+            music.playTone(Note.D3, music.beat(90))
+        }
+    } else {
+        
+        x = randint(0, 10)
+        for (index = 0; index < x; index++) {
+            music.playTone(Note.D3, music.beat(90))
+        }
+    }
+    
 })
